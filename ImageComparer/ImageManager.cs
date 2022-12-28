eb43055737182db8323bcd5672e4cb682b645543
Gusev Maksim <mgf_mgf_59@mail.ru>
2022-12-28 16:06:49 +0400

    Добавьте файлы проекта.

---
+++ ImageComparer/ImageManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ImageComparer
+{
+    public class ImageManager
+    {
+        public static bool CompareImages(string image1Path, string image2Path)
+        {
+            // Получаем хэши картинок с помощью функции GetImageHash
+            var image1Hash = GetImageHash(image1Path);
+            var image2Hash = GetImageHash(image2Path);
+
+            // Сравниваем хэши
+            return image1Hash == image2Hash;
+        }
+
+        private static string GetImageHash(string imagePath)
+        {
+            // Читаем картинку в поток
+            using (var stream = new FileStream(imagePath, FileMode.Open))
+            {
+                // Создаем хэширующую функцию
+                var sha1 = SHA1.Create();
+
+                // Вычисляем хэш картинки
+                var hash = sha1.ComputeHash(stream);
+
+                // Конвертируем хэш в строку и возвращаем
+                return BitConverter.ToString(hash).Replace("-", string.Empty);
+            }
+        }
+    }
+}
---
+++ ImageComparer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ImageComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ResetColor();
+            Console.WriteLine("Введите путь до 1 картинки");
+            var path1 = Console.ReadLine();
+            Console.WriteLine("Введите путь до 2 картинки");
+            var path2 = Console.ReadLine();
+
+            var result = ImageManager.CompareImages(path1, path2);
+            if (result)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Картинки идентичны");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Картинки разные");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
+
